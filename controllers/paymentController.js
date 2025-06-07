0384fb4c035d772e993f2867f239a532c3b50cba
khubaib shah <bewithbaacha@gmail.com>
2025-06-08 00:19:36 +0500

    fix: correct URL formatting in PayPal redirect URLs

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -54,8 +54,8 @@ export const payForOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: `${process.env.CLIENT_URL}payment-success?orderId=${savedOrder._id}`,
-        cancel_url: `${process.env.CLIENT_URL}payment-cancelled`,
+        return_url: `${process.env.CLIENT_URL}/payment-success?orderId=${savedOrder._id}`,
+        cancel_url: `${process.env.CLIENT_URL}/payment-cancelled`,
       },
       transactions: [
         {
